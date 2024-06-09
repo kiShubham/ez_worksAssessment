635d596c2b2d50a9a649a64c99be7ba2ede66e46
Shubham singh <shubhamshubhank1997@gmail.com>
2024-06-09 16:11:44 +0530

    using allOrigin Proxyservice

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
-const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"; // using proxy as api link given is http not https ;
+const CORS_PROXY = "https://api.allorigins.win/get?url="; // using proxy as api link given is http not https ;
 export const Backend_endpoint = "http://34.225.132.160:8002/api";
 
 export const postEmailreq = async (data) => {
   try {
-    const res = await axios.post(`${CORS_PROXY}${Backend_endpoint}`, data);
-    // console.log(res);
+    const res = await axios.post(
+      `${CORS_PROXY}${encodeURIComponent(Backend_endpoint)}`,
+      data
+    );
     return res;
   } catch (error) {
     return error.response;
   }
 };
 
-console.log(`${CORS_PROXY}${Backend_endpoint}`);
+// console.log(`${CORS_PROXY}${Backend_endpoint}`);
 // response codes
 // 200 successfull or 422 not valid
