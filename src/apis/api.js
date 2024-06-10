8a43115eff9ca2f4f6696c1d54c531c6f5a56eba
Shubham singh <shubhamshubhank1997@gmail.com>
2024-06-10 13:12:38 +0530

    changed proxy

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,33 @@
 import axios from "axios";
 
-// https://cors-anywhere.herokuapp.com/corsdemo ,
+const CORS_sh_PROXY = "https://proxy.cors.sh/"; //https://cors.sh/
+
+const Api_endpoint = "http://34.225.132.160:8002/api";
+
+const headerObj = {
+  headers: {
+    "x-cors-api-key": "temp_ded85bc3c53176589cafa9a0077db77b",
+  },
+};
+
+export const postEmailreq = async (data) => {
+  try {
+    const res = await axios.post(
+      `${CORS_sh_PROXY}${Api_endpoint}`,
+      data,
+      headerObj
+    );
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error.response);
+    return error.response;
+  }
+};
+
+/* 
+  
+//* https://cors-anywhere.herokuapp.com/corsdemo ,
 // visit above link in the browser you are runnig the app and request for temporary access ;
 
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
@@ -18,6 +45,4 @@ export const postEmailreq = async (data) => {
   }
 };
 
-// console.log(`${CORS_PROXY}${Api_endpoint}`);
-// response codes
-// 200 successfull or 422 not valid
+ */
